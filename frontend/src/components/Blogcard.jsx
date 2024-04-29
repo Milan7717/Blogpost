47132e0f61ff8d41ad3cbc7962f33e5980ef689d
milan mishra <milan1mishra2@gmail.com>
2024-04-29 21:16:56 +0545

    blog deleted  succesfully using delete  method

--- frontend/src/components/Blogcard.jsx
+++ frontend/src/components/Blogcard.jsx
@@ -5,19 +5,23 @@ import { MdOutlineDelete } from "react-icons/md";
 
 const Blogcard = (props) => {
   return (
-    <div className="border-[1px] rounded-xl border-gray-[500] h-[40vh] w-full flex flex-col gap-4 justify-between overflow-hidden ">
-     <div className="flex flex-col gap-10">
-      <p className="text-4xl">{props.title}</p>
-
-      <p className="">{props.content}</p>
+    <div className="border-[1px] rounded-xl border-gray-600 h-[40vh] w-full flex flex-col gap-4 justify-between overflow-hidden ">
+      <div className="flex flex-col gap-4">
+        <div className="my-2">
+          <p className="text-4xl text-center capitalize">{props.title}</p>
+        </div>
+        <div className="p-2">
+          
+          <p className="line-clamp-4">{props.content}</p>
+        </div>
       </div>
-      <div className="flex justify-center gap-x-6 my-4" >
+      <div className="flex justify-center gap-x-6 my-4">
         <Link to={`/editblog/${props.id}`}>
-        <AiOutlineEdit className="text-2xl text-yellow-600" />
+          <AiOutlineEdit className="text-2xl text-yellow-600" />
         </Link>
         <Link to={`/deleteblog/${props.id}`}>
-            <MdOutlineDelete className="text-2xl text-red-600" />
-          </Link>
+          <MdOutlineDelete className="text-2xl text-red-600" />
+        </Link>
       </div>
     </div>
   );
--- frontend/src/pages/Deleteblog.jsx
+++ frontend/src/pages/Deleteblog.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 
@@ -7,20 +8,20 @@ const Deleteblog = () => {
 
     const handleDelete = async () => {
         try {
-          await axios.delete(`https://shoeshop-mern.onrender.com/category/${id}`)
+          await axios.delete(`http://localhost:3000/blog/${id}`)
           alert("deleted successfully");
-          navigate("/admincategory");
+          navigate("/home");
         } catch (error) {
           console.log(error);
-          alert("Failed to delete category");
+          alert("Failed to delete Blog");
         }
       };
   return (
     <>
-      <h1 className="text-3xl my-4">Delete Category</h1>
+      <h1 className="text-3xl my-4">Delete Blog Post</h1>
       <div className="flex flex-col item-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
         <h3 className="text-2xl text-center">
-          Are you sure you want to delete this Category?
+          Are you sure you want to delete this Blog Post?
         </h3>
 
         <button
